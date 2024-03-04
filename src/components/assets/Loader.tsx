60609d2a77cc692764e8c70addd5f4575a8c12c2
Herbert Duarte <herbertduarte.santos@gmail.com>
2024-03-04 11:15:56 -0300

    fix compact on loader

--- src/components/assets/Loader.tsx
+++ src/components/assets/Loader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Loader({ compact = false }: { compact: boolean }) {
+export default function Loader({ compact }: { compact?: boolean }) {
   return compact ? (
     <div className="flex-col gap-4 w-full flex items-center justify-center">
       <div className="w-14 h-14 border-8 text-priborder-t-primary-dark animate-spin border-gray-200 flex items-center justify-center border-t-violet-500 rounded-full" />
